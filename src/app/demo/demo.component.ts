0802ff657beea690d65eec8046c7d38af0b8b3f0
Dharati-Kamble <dkamble031993@gmail.com>
2024-11-29 19:35:21 +0530

    Initial commit for Arithmetic App

---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ArithmeticService } from '../arithmetic.service';
+
+@Component({
+  selector: 'app-demo',
+  standalone: true,
+  imports: [],
+  templateUrl: './demo.component.html',
+  styleUrl: './demo.component.css'
+})
+export class DemoComponent {
+  additionResult: number = 0;
+  subtractionResult: number = 0;
+  constructor(private arithmeticService: ArithmeticService) {}
+
+  ngOnInit(): void {
+    const num1 = 10;
+    const num2 = 5;
+
+    this.additionResult = this.arithmeticService.Add(num1, num2);
+    this.subtractionResult = this.arithmeticService.Sub(num1, num2);
+  }
+}
